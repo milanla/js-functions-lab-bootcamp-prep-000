1db571481caedb09fe57f3e9b6f747b028928f59
Milan Sacher <milan.lanlada@gmail.com>
2018-05-24 23:13:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+/*
+
 function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
-}
+} */
